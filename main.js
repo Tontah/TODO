a6f084926c75ced708f531a0e520ef51a2d479a4
Tontah <nikitatchana6@gmail.com>
2022-08-30 10:27:26 +0200

    deleteButton and checkbox added and functioning

--- main.js
+++ main.js
@@ -16,30 +16,81 @@ document.addEventListener('DOMContentLoaded', () => {
             }
                 addTodo(newToDoInput.value);
                 displayingToDos(newToDoInput.value);
-                todoCount();
+                incrementTodoCount();
                 newToDoInput.value = "";
             }
         }
          console.log(event.target.value);
     });
     //This function increases the todo count
-    function todoCount(){
+    function incrementTodoCount(){
         newToDoCount++;
+        document.querySelector(".todo-count").innerHTML=newToDoCount + " Item(s) left";
+
+    }
+    //This function decreases the todocount
+    function decrementTodoCount(){
+        newToDoCount--;
         document.querySelector(".todo-count").innerHTML=newToDoCount + " Item(s) left";
 
     }
 // This function pushes the text into the todo list
     function addTodo(text) {
-        todoItems.push(text);
+        let todo={
+            task: text,
+            completed: false
+        }
+        todoItems.push(todo);
         console.log(todoItems);
     }
 //Showing the todolist
+    //The SPAn and div difference?????
     function displayingToDos(text){
         let todolistNode = document.createElement("li");
+        let deleteButton = document.createElement("SPAN");
+        deleteButton.className=("deleteButton");
+        let completeCheckbox = document.createElement("div");
+        completeCheckbox.classList.add("completeCheckbox");
+        let remove = document.createElement("button");
+        remove.classList.add("remove");
+        remove.innerHTML = "\u00D7";
+        remove.addEventListener("click", deleteTask)
+        let complete =document.createElement("input");
+        complete.type = "checkbox";
+        complete.classList.add("complete")
+        complete.addEventListener("click", completeTask)
+        deleteButton.appendChild(remove)
+        completeCheckbox.appendChild(complete)
+        todolistNode.appendChild(complete);
         todolistNode.appendChild(document.createTextNode(text));
+        todolistNode.appendChild(deleteButton);
         todolist.appendChild(todolistNode);
+
     }
-    // Add a "checked" symbol when clicking on a list item
+    function deleteTask() {
+        let deleteTODO = this.parentNode.parentNode
+        console.log(deleteTODO);
+        // grab the `ul` (li -> ul)
+        let parent = deleteTODO.parentNode
+        // remove `li` from `ul`
+        parent.removeChild(deleteTODO)
+        //deleting the task from the array as well
+        //console.log("deleteTODO.textContent1= "+ deleteTODO.textContent);
+        for (let i = 0; i < todoItems.length; i++) {
+            //compares if the item[i] of the array equals the element to be deleted
+            //why is the delete button representative present in my tasktext??
+            if ((todoItems[i].task+"\u00D7") === deleteTODO.textContent) {
+                todoItems.splice(i, 1);
+                //console.log("deleteTODO.textContent2= "+ deleteTODO.textContent);
+                //console.log("\u00D7+todoItems[i].task= "+ "\u00D7"+todoItems[i].task);
+            }
+        }
+            decrementTodoCount()
+        console.log(todoItems);
+    }
+function completeTask(){
+
+}
 
 })
 
